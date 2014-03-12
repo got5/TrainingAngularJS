1e42e16eeefd731c0550a3a7f66b8db7cab7b2b6
Krazar <krazarpwnu@gmail.com>
2014-03-12 23:01:42 +0100

    step3

--- app/js/controllers/DetailController.js
+++ app/js/controllers/DetailController.js
@@ -15,15 +15,50 @@
 
             /** Add select item to user cart. */
             $scope.addToCart = function () {
-                // TODO - not for first tp
+                // TODO
             };
 
-            $scope.getImage = function(id){
-                // Not mandatory but allow to avoid an error in the console
-            }
+            $scope.getImage = function (id) {
+                if (!id) {
+                    return "";
+                } else {
+                    return "/img/catalog/" + id + ".jpg";
+                }
+            };
 
             $scope.getCSSRating = function (comments) {
-               //TODO
+                if (!comments) {
+                    return [];
+                } else {
+                    var rating = 0;
+                    for (var i = 0; i < comments.length; i++) {
+                        rating += comments[i].rate;
+                    }
+                    rating = Math.floor(rating / comments.length);
+
+                    var css = ['rating'];
+
+                    switch (rating) {
+                        case 1:
+                            css.push("one");
+                            break;
+                        case 2:
+                            css.push("two");
+                            break;
+                        case 3:
+                            css.push("three");
+                            break;
+                        case 4:
+                            css.push("four");
+                            break;
+                        case 5:
+                            css.push("five");
+                            break;
+                        default :
+                            css.push("zero");
+                    }
+                    return css;
+                }
             };
         }]);
 }());
