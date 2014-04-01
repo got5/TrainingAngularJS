0fe8d92f5f5c964587fff477edd96fa29b6d7d0c
Krazar <krazarpwnu@gmail.com>
2014-04-01 18:37:56 +0200

    init

--- app/js/controllers/CatalogController.js
+++ app/js/controllers/CatalogController.js
@@ -5,18 +5,9 @@
     angular.module('app')
         .controller('CatalogController', ['$scope', '$location', 'catalogService','ProductUtils', 'UserService', function ($scope, $location, catalogService, ProductUtils, UserService) {
 
-
-
             /** Returns all products. */
             catalogService.getCatalog().success(function (result) {
                 $scope.products = result;
             });
-
-            $scope.getRatingClass = ProductUtils.getRatingCss;
-
-            $scope.addToCart = function(pItem){
-                 UserService.addToCart(pItem,1);
-                 $location.path('/basket');
-            };
         }]);
 }());
