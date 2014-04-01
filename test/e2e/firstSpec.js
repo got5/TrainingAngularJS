4d03a671fa63d2c91bbbed3805a2e0d6152582c1
Krazar <krazarpwnu@gmail.com>
2014-04-01 21:45:49 +0200

    dumb e2e test

--- test/e2e/firstSpec.js
+++ test/e2e/firstSpec.js
@@ -2,3 +2,9 @@
  * Created by pierremarot on 01/04/2014.
  */
 
+describe("Detail Book Page", function () {
+
+    beforeEach(function () {
+        browser.get('http://localhost:3000/#/book/1');
+    });
+});
