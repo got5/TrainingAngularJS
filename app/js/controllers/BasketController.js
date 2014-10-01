915988d392cdf2f3aaa2e051888c7d5232fcb07e
ffacon <francois.facon@gmail.com>
2014-10-01 16:28:18 +0200

    Merge from step2

--- app/js/controllers/BasketController.js
+++ app/js/controllers/BasketController.js
@@ -4,8 +4,8 @@
     /** Basket view controller */
     angular.module('app')
         .controller('BasketController', ['$scope', function ($scope) {
-            $scope.items//todo;
-            $scope.nbItem//todo;
+            $scope.items = [];//todo
+            $scope.nbItem = 0; //todo
             $scope.getTotal = function () {
                 var total = 0;
                 for(var index = 0 ; index < $scope.items.length ; index++){
