1219003dc5efc34971d1ff784546b695232515b7
françois facon <francois.facon@atos.net>
2014-12-18 15:17:49 +0100

    Merge Fix Step8

--- app/js/controllers/CatalogController.js
+++ app/js/controllers/CatalogController.js
@@ -10,13 +10,6 @@
                 $scope.products = result;
             });
 
-            $scope.getRatingClass = ProductUtils.getRatingCss;
-
-            $scope.addToCart = function(pItem){
-                 UserService.addToCart(pItem,1);
-                 $location.path('/basket');
-            };
-
             $scope.nbResults= 5;
         }]);
 })();
