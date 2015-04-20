b99f531108818dae303c38046d22fd5ac66c0f14
a571301 <regis.legrand@worldline.com>
2015-04-20 11:28:36 +0200

    new slides

---
+++ js/app.js
@@ -0,0 +1,1 @@
+angular.module("trainingAJ",["sdco-slides"]).config(["slidesConfigProvider","$controllerProvider",function(n,i){n.applyConf(),angular.module("trainingAJ").exoController=i.register}]).run(["slidesConfig",function(n){n.init()}]);
