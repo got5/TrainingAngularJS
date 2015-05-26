3347478ceba19c78615490b6f4481992f82e1183
a571301 <regis.legrand@worldline.com>
2015-05-26 16:59:02 +0200

    fix initial files

--- app/js/directives/mainLayout.js
+++ app/js/directives/mainLayout.js
@@ -4,14 +4,16 @@
     var app = angular.module('app');
 
     /** Layout directive controller */
-    var LayoutController = function ($scope, $cookies, $rootScope, $location, $route) {
+    var LayoutController = function ($scope, $cookies, $rootScope, $location, $route, UserService) {
 
-        // TODO: This function has to be called when:
-        // -> The user is logged in
-        // -> The user is logged out
+        $scope.$on('$routeChangeSuccess', function(){
+            $scope.updateLayoutData();
+        });
+
+        // TODO: Set the logged user in the scope
         $scope.updateLayoutData= function(){
 
-        }
+        };
 
     };
 
@@ -22,9 +24,9 @@
             replace: true,
             templateUrl: "templates/partials/mainLayout.html",
             controller: [
-                '$scope', '$cookies', '$rootScope', '$location', '$route',
-                function ($scope, $cookies, $rootScope, $location, $route) {
-                    return new LayoutController($scope, $cookies, $rootScope, $location, $route);
+                '$scope', '$cookies', '$rootScope', '$location', '$route','UserService',
+                function ($scope, $cookies, $rootScope, $location, $route, UserService) {
+                    return new LayoutController($scope, $cookies, $rootScope, $location, $route, UserService);
                 }]
         };
     });
