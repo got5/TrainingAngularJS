9260c010aff1e147693d58c2a238203fbcbac3c9
a571301 <regis.legrand@worldline.com>
2015-05-29 10:23:42 +0200

    rest easy impl

---
+++ jetty-resteasy/src/main/java/com/example/beans/News.java
@@ -0,0 +1,41 @@
+package com.example.beans;
+
+public class News {
+	
+	private int id;
+	private String author;
+	private String category;
+	private String content;
+	private int likes;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+}
