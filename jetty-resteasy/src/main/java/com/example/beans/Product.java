0fe00a3ef91ab06100d6c299736e42f2bfd50c51
a571301 <regis.legrand@worldline.com>
2015-05-29 10:23:42 +0200

    rest easy impl

---
+++ jetty-resteasy/src/main/java/com/example/beans/Product.java
@@ -0,0 +1,64 @@
+package com.example.beans;
+
+public class Product {
+	
+	int id;
+	String name;
+	String author;
+	double price;
+	String description;
+	String category;
+	boolean isNew;
+	ProductComment[] comments;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public boolean getIsNew() {
+		return isNew;
+	}
+	public void setIsNew(boolean isNew) {
+		this.isNew = isNew;
+	}
+	public ProductComment[] getComments() {
+		return comments;
+	}
+	public void setComments(ProductComment[] comments) {
+		this.comments = comments;
+	}
+	
+	
+
+}
